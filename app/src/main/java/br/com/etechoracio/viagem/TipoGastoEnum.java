ccb01d9249495a895c8e671531a53e2a4cb2a94d
Ismael Veloso <velosoismael1@gmail.com>
2019-04-16 20:42:16 -0300

    Ex.18 - Spinner

---
+++ app/src/main/java/br/com/etechoracio/viagem/TipoGastoEnum.java
@@ -0,0 +1,16 @@
+package br.com.etechoracio.viagem;
+
+public enum TipoGastoEnum {
+    ALIMENTACAO("Alimentação"),
+    COMBUSTIVEL("Combustivel"),
+    TRANSPORTE("Transporte"),
+    HOSPEDAGEM("Hospedagem"),
+    OUTROS("Outros");
+
+
+    private String descricao;
+
+    TipoGastoEnum(String descricao) {
+        this.descricao = descricao;
+    }
+}
